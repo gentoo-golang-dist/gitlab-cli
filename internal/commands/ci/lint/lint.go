5304d5e0d2fd03eeb49181c3a54e51dac238f347
thisisrick25 <swapnaneel06@gmail.com>
2026-01-23 21:38:28 +0530

    feat: add component context flags and interpolation for CI/CD linting

--- internal/commands/ci/lint/lint.go
+++ internal/commands/ci/lint/lint.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"regexp"
 
 	"github.com/MakeNowJust/heredoc/v2"
 	"github.com/spf13/cobra"
@@ -28,6 +29,12 @@ type options struct {
 	ref         string
 	dryRun      bool
 	includeJobs bool
+
+	// Component context values for interpolation
+	componentName      string
+	componentVersion   string
+	componentSHA       string
+	componentReference string
 }
 
 func NewCmdLint(f cmdutils.Factory) *cobra.Command {
@@ -45,6 +52,9 @@ func NewCmdLint(f cmdutils.Factory) *cobra.Command {
 			$ glab ci lint
 			$ glab ci lint .gitlab-ci.yml
 			$ glab ci lint path/to/.gitlab-ci.yml
+
+			# Lint a CI/CD component template with component context values
+			$ glab ci lint templates/my-component.yml --component-name=my-component --component-version=1.0.0
 		`),
 		Annotations: map[string]string{
 			mcpannotations.Safe: "true",
@@ -59,6 +69,12 @@ func NewCmdLint(f cmdutils.Factory) *cobra.Command {
 	pipelineCILintCmd.Flags().BoolVarP(&opts.includeJobs, "include-jobs", "", false, "Response includes the list of jobs that would exist in a static check or pipeline simulation.")
 	pipelineCILintCmd.Flags().StringVar(&opts.ref, "ref", "", "When 'dry-run' is true, sets the branch or tag context for validating the CI/CD YAML configuration.")
 
+	// Component context flags for linting CI/CD component templates
+	pipelineCILintCmd.Flags().StringVar(&opts.componentName, "component-name", "", "Component name to use for $[[ component.name ]] interpolation.")
+	pipelineCILintCmd.Flags().StringVar(&opts.componentVersion, "component-version", "", "Component version to use for $[[ component.version ]] interpolation.")
+	pipelineCILintCmd.Flags().StringVar(&opts.componentSHA, "component-sha", "", "Component SHA to use for $[[ component.sha ]] interpolation.")
+	pipelineCILintCmd.Flags().StringVar(&opts.componentReference, "component-reference", "", "Component reference to use for $[[ component.reference ]] interpolation.")
+
 	return pipelineCILintCmd
 }
 
@@ -68,6 +84,35 @@ func (o *options) complete(args []string) {
 	} else {
 		o.path = ".gitlab-ci.yml"
 	}
+}
+
+// replaceComponentContext replaces $[[ component.* ]] expressions with the provided values.
+// This allows linting CI/CD component templates that use component context metadata.
+func (o *options) replaceComponentContext(content string) string {
+	// Build a map of component context values
+	replacements := map[string]string{
+		"name":      o.componentName,
+		"version":   o.componentVersion,
+		"sha":       o.componentSHA,
+		"reference": o.componentReference,
+	}
+
+	// Replace each component context expression if a value was provided
+	for field, value := range replacements {
+		if value != "" {
+			// Match $[[ component.field ]] with optional whitespace
+			pattern := regexp.MustCompile(`\$\[\[\s*component\.` + regexp.QuoteMeta(field) + `\s*\]\]`)
+			content = pattern.ReplaceAllString(content, value)
+		}
+	}
+
+	return content
+}
+
+// hasComponentContext checks if any component context flags were provided
+func (o *options) hasComponentContext() bool {
+	return o.componentName != "" || o.componentVersion != "" ||
+		o.componentSHA != "" || o.componentReference != ""
 }
 
 func (o *options) run() error {
@@ -117,8 +162,14 @@ func (o *options) run() error {
 
 	fmt.Fprintln(o.io.StdOut, "Validating...")
 
+	// Replace component context expressions if any component context flags were provided
+	contentStr := string(content)
+	if o.hasComponentContext() {
+		contentStr = o.replaceComponentContext(contentStr)
+	}
+
 	lintOpts := &gitlab.ProjectNamespaceLintOptions{
-		Content:     gitlab.Ptr(string(content)),
+		Content:     gitlab.Ptr(contentStr),
 		DryRun:      gitlab.Ptr(o.dryRun),
 		IncludeJobs: gitlab.Ptr(o.includeJobs),
 	}
