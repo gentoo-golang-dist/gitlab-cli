af92cf913d80af0dfa209f7bc0ec411ed20885be
thisisrick25 <swapnaneel06@gmail.com>
2026-01-28 14:44:05 +0530

    fix: update command help output to use angle brackets for usernames and repository formats

--- internal/cmdutils/repo_override.go
+++ internal/cmdutils/repo_override.go
@@ -9,7 +9,7 @@ import (
 func EnableRepoOverride(cmd *cobra.Command, f Factory) {
 	// if the flag doesn't exist yet, create it
 	if cmd.PersistentFlags().Lookup("repo") == nil {
-		cmd.PersistentFlags().StringP("repo", "R", "", "Select another repository. Can use either `OWNER/REPO` or `GROUP/NAMESPACE/REPO` format. Also accepts full URL or Git URL.")
+		cmd.PersistentFlags().StringP("repo", "R", "", "Select another repository. Can use either <OWNER/REPO> or <GROUP/NAMESPACE/REPO> format. Also accepts full URL or Git URL.")
 	}
 
 	// unhide the flag for this command and its children
@@ -34,7 +34,7 @@ func EnableRepoOverride(cmd *cobra.Command, f Factory) {
 
 // AddGlobalRepoOverride adds the -R flag globally but keeps it hidden
 func AddGlobalRepoOverride(cmd *cobra.Command, f Factory) {
-	cmd.PersistentFlags().StringP("repo", "R", "", "Select another repository. Can use either `OWNER/REPO` or `GROUP/NAMESPACE/REPO` format. Also accepts full URL or Git URL.")
+	cmd.PersistentFlags().StringP("repo", "R", "", "Select another repository. Can use either <OWNER/REPO> or <GROUP/NAMESPACE/REPO> format. Also accepts full URL or Git URL.")
 	_ = cmd.PersistentFlags().MarkHidden("repo")
 
 	originalPreRunE := cmd.PersistentPreRunE
