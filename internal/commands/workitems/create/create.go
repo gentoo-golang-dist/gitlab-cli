118e0a4d24ec45b706debf4aad6449c8bd868fb7
Carlos Corona <ccorona@gitlab.com>
2026-03-08 17:02:42 -0700

    feat(workitems): add work-items create cmd skeleton with type utilities and made `WorkItemTypeIDs` map unexported based on Duo's feedback

---
+++ internal/commands/workitems/create/create.go
@@ -0,0 +1,87 @@
+package create
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/MakeNowJust/heredoc/v2"
+	"github.com/charmbracelet/huh"
+	"github.com/spf13/cobra"
+	gitlab "gitlab.com/gitlab-org/api/client-go/v2"
+	"gitlab.com/gitlab-org/cli/internal/cmdutils"
+	"gitlab.com/gitlab-org/cli/internal/commands/workitems/utils"
+	"gitlab.com/gitlab-org/cli/internal/config"
+	"gitlab.com/gitlab-org/cli/internal/glrepo"
+	"gitlab.com/gitlab-org/cli/internal/iostreams"
+	"gitlab.com/gitlab-org/cli/internal/mcpannotations"
+	"gitlab.com/gitlab-org/cli/internal/text"
+)
+
+type options struct {
+	// Dependencies
+	io           *iostreams.IOStreams
+	baseRepo     func() (glrepo.Interface, error)
+	gitlabClient func() (*gitlab.Client, error)
+	config       func() config.Config
+
+	// Flags
+	group        string
+	title        string
+	workItemType string
+	description  string
+	confidential bool
+
+	// Internal state
+	needsPrompt bool
+}
+
+func NewCmd(f cmdutils.Factory) *cobra.Command {
+	opts := &options{
+		io:           f.IO(),
+		baseRepo:     f.BaseRepo,
+		gitlabClient: f.GitLabClient,
+		config:       f.Config,
+	}
+
+	cmd := &cobra.Command{
+		Use:   "create [flags]",
+		Short: "Create work items in a project or group. (EXPERIMENTAL)",
+		Long: heredoc.Doc(`Create work items in a project or group.
+
+Automatically detects scope from repository context. 
+Use --group flag for group-level work items or -R to specify a different project.
+`) + text.ExperimentalString,
+		Aliases: []string{"new"},
+		Example: heredoc.Doc(`
+										# Create work item in current project
+										glab work-items create --type issue
+		`),
+		Args: cobra.ExactArgs(0),
+		Annotations: map[string]string{
+			mcpannotations.Destructive: "true",
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := opts.complete(cmd); err != nil {
+				return err
+			}
+			if err := opts.validate(); err != nil {
+				return err
+			}
+			return opts.run(cmd.Context())
+		},
+	}
+
+	// Enable -R flag for repo override
+	cmdutils.EnableRepoOverride(cmd, f)
+
+	// Flags
+	cmd.Flags().StringP("group", "g", "", "Create work items for a group or subgroup")
+	cmd.Flags().StringVarP(&opts.workItemType, "type", "T", "", "Type of work item ("+strings.Join(utils.ValidTypeNames(), ", ")+").")
+
+	cmd.Flags().StringVarP(&opts.title, "title", "t", "", "Add title for work item")
+	cmd.Flags().StringVarP(&opts.description, "description", "d", "", "Description of the work item. Set to \"-\" to open an editor.")
+	cmd.Flags().BoolVarP(&opts.confidential, "confidential", "c", false, "Mark work item confidential.")
+
+	return cmd
+}
