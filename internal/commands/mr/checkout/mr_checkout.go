ca3a298947959b466e32b6ae8c7f83315bff882d
Gary Holtz <gholtz@gitlab.com>
2026-03-16 15:34:50 -0500

    refactor(mr/checkout): inject GitRunner dependency for testability

--- internal/commands/mr/checkout/mr_checkout.go
+++ internal/commands/mr/checkout/mr_checkout.go
@@ -25,7 +25,7 @@ type mrCheckoutConfig struct {
 
 var mrCheckoutCfg mrCheckoutConfig
 
-func NewCmdCheckout(f cmdutils.Factory) *cobra.Command {
+func NewCmdCheckout(f cmdutils.Factory, gr git.GitRunner) *cobra.Command {
 	mrCheckoutCmd := &cobra.Command{
 		Use:   "checkout [<id> | <branch> | <url>]",
 		Short: "Check out an open merge request.",
@@ -108,10 +108,10 @@ func NewCmdCheckout(f cmdutils.Factory) *cobra.Command {
 			repoURL := glrepo.RemoteURL(mrProject, gitProtocol)
 
 			fetchRefSpec := fmt.Sprintf("%s:%s", mrRef, mrCheckoutCfg.branch)
-			if err := git.RunCmd([]string{"fetch", repoURL, fetchRefSpec}); err != nil {
+			if _, err := gr.Git("fetch", repoURL, fetchRefSpec); err != nil {
 				// the remote may have diverged from local after git operations
 				// try fetching without updating the branch ref before giving up
-				if err := git.RunCmd([]string{"fetch", repoURL, mrRef}); err != nil {
+				if _, err := gr.Git("fetch", repoURL, mrRef); err != nil {
 					return err
 				}
 			}
@@ -119,28 +119,24 @@ func NewCmdCheckout(f cmdutils.Factory) *cobra.Command {
 			// .remote is needed for `git pull` to work
 			// .pushRemote is needed for `git push` to work, if user has set `remote.pushDefault`.
 			// see https://git-scm.com/docs/git-config#Documentation/git-config.txt-branchltnamegtremote
-			if err := git.RunCmd([]string{"config", fmt.Sprintf("branch.%s.remote", mrCheckoutCfg.branch), repoURL}); err != nil {
+			if _, err := gr.Git("config", fmt.Sprintf("branch.%s.remote", mrCheckoutCfg.branch), repoURL); err != nil {
 				return err
 			}
 			if mr.AllowCollaboration {
-				if err := git.RunCmd([]string{"config", fmt.Sprintf("branch.%s.pushRemote", mrCheckoutCfg.branch), repoURL}); err != nil {
+				if _, err := gr.Git("config", fmt.Sprintf("branch.%s.pushRemote", mrCheckoutCfg.branch), repoURL); err != nil {
 					return err
 				}
 			}
-			if err := git.RunCmd([]string{"config", fmt.Sprintf("branch.%s.merge", mrCheckoutCfg.branch), mrRef}); err != nil {
+			if _, err := gr.Git("config", fmt.Sprintf("branch.%s.merge", mrCheckoutCfg.branch), mrRef); err != nil {
 				return err
 			}
 
-			// Check out branch
-			var gr git.StandardGitCommand
-
-			if err := git.CheckoutBranch(mrCheckoutCfg.branch, gr); err != nil {
+			if _, err := gr.Git("checkout", mrCheckoutCfg.branch); err != nil {
 				return err
 			}
 
-			// Check out the branch
 			if upstream != "" {
-				if err := git.RunCmd([]string{"branch", "--set-upstream-to", upstream}); err != nil {
+				if _, err := gr.Git("branch", "--set-upstream-to", upstream); err != nil {
 					return err
 				}
 			}
--- internal/commands/mr/mr.go
+++ internal/commands/mr/mr.go
@@ -25,6 +25,7 @@ import (
 	mrUnsubscribeCmd "gitlab.com/gitlab-org/cli/internal/commands/mr/unsubscribe"
 	mrUpdateCmd "gitlab.com/gitlab-org/cli/internal/commands/mr/update"
 	mrViewCmd "gitlab.com/gitlab-org/cli/internal/commands/mr/view"
+	"gitlab.com/gitlab-org/cli/internal/git"
 )
 
 func NewCmdMR(f cmdutils.Factory) *cobra.Command {
@@ -50,7 +51,8 @@ func NewCmdMR(f cmdutils.Factory) *cobra.Command {
 
 	mrCmd.AddCommand(mrApproveCmd.NewCmdApprove(f))
 	mrCmd.AddCommand(mrApproversCmd.NewCmdApprovers(f))
-	mrCmd.AddCommand(mrCheckoutCmd.NewCmdCheckout(f))
+	var gr git.StandardGitCommand
+	mrCmd.AddCommand(mrCheckoutCmd.NewCmdCheckout(f, gr))
 	mrCmd.AddCommand(mrCloseCmd.NewCmdClose(f))
 	mrCmd.AddCommand(mrCreateCmd.NewCmdCreate(f))
 	mrCmd.AddCommand(mrDeleteCmd.NewCmdDelete(f))
