ef75fd35d302af9d85171ccd2ddacb9e0e301a4e
Brendan Lynch <blynch@gitlab.com>
2026-05-19 14:14:22 +0200

    docs: improve glab changelog command docs

--- internal/commands/changelog/changelog.go
+++ internal/commands/changelog/changelog.go
@@ -1,6 +1,7 @@
 package changelog
 
 import (
+	"github.com/MakeNowJust/heredoc/v2"
 	"github.com/spf13/cobra"
 
 	"gitlab.com/gitlab-org/cli/internal/cmdutils"
@@ -10,8 +11,15 @@ import (
 func NewCmdChangelog(f cmdutils.Factory) *cobra.Command {
 	changelogCmd := &cobra.Command{
 		Use:   "changelog <command> [flags]",
-		Short: `Interact with the changelog API.`,
-		Long:  ``,
+		Short: `Generate changelogs from your project's commit history.`,
+		Long: heredoc.Doc(`
+		Generate changelogs from commits in your project's Git repository.
+		Changelog entries are built from commits that include a configured Git
+		trailer, such as 'Changelog'.
+
+		For more information about how GitLab generates changelogs, see
+		[Changelogs](https://docs.gitlab.com/user/project/changelogs/).
+		`),
 	}
 
 	// Subcommands
