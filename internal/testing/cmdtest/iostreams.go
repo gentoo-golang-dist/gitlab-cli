32596be9daf140d5d41273c52f52d9bdba89ba28
Brendan Lynch <blynch@gitlab.com>
2026-05-19 10:53:27 +0200

    chore: Apply 2 suggestion(s) to 1 file(s)

--- internal/testing/cmdtest/iostreams.go
+++ internal/testing/cmdtest/iostreams.go
@@ -59,11 +59,7 @@ func TestIOStreams(options ...iostreams.IOStreamsOption) (*iostreams.IOStreams, 
 	opts := []iostreams.IOStreamsOption{
 		iostreams.WithStdin(io.NopCloser(in), false),
 		iostreams.WithStdout(out, false),
-
 		iostreams.WithStderr(errOut, false),
-		// Disable hyperlinks by default in tests to avoid OSC 8 escape sequences
-		// in test output. Tests that explicitly test hyperlink behavior should pass
-		// iostreams.WithDisplayHyperLinks("auto") or iostreams.WithDisplayHyperLinks("always").
 		iostreams.WithDisplayHyperLinks("never"),
 	}
 	opts = append(opts, options...)
