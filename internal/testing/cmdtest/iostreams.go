f387bb4a5253dc5d1512ee771c1894f12fecca3c
Brendan Lynch <blynch@gitlab.com>
2026-05-19 10:28:41 +0200

    fix: disable hyperlinks in test IO streams

--- internal/testing/cmdtest/iostreams.go
+++ internal/testing/cmdtest/iostreams.go
@@ -59,7 +59,12 @@ func TestIOStreams(options ...iostreams.IOStreamsOption) (*iostreams.IOStreams, 
 	opts := []iostreams.IOStreamsOption{
 		iostreams.WithStdin(io.NopCloser(in), false),
 		iostreams.WithStdout(out, false),
+
 		iostreams.WithStderr(errOut, false),
+		// Disable hyperlinks by default in tests to avoid OSC 8 escape sequences
+		// in test output. Tests that explicitly test hyperlink behavior should pass
+		// iostreams.WithDisplayHyperLinks("auto") or iostreams.WithDisplayHyperLinks("always").
+		iostreams.WithDisplayHyperLinks("never"),
 	}
 	opts = append(opts, options...)
 
