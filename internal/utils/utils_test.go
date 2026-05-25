cf480f0841a51776a80c55f37a8a6d1d1f3d95d1
Filip Aleksic <faleksic@gitlab.com>
2026-05-25 10:38:08 +0200

    chore: Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: GitLab Duo <gitlab-duo@gitlab.com>

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -286,15 +286,15 @@ func Test_HelperFuncs(t *testing.T) {
 	t.Run("StringToInt()", func(t *testing.T) {
 		t.Run("success", func(t *testing.T) {
 			got := StringToInt("200")
-			assert.Equal(t, 200, got)
+			require.Equal(t, 200, got)
 		})
 		t.Run("failed-return-0", func(t *testing.T) {
 			got := StringToInt("NotAnInt")
-			assert.Equal(t, 0, got)
+			require.Equal(t, 0, got)
 		})
 	})
 	t.Run("ReplaceNonAlphaNumericChars()", func(t *testing.T) {
 		got := ReplaceNonAlphaNumericChars("profclems-glab", "/")
-		assert.Equal(t, "profclems/glab", got)
+		require.Equal(t, "profclems/glab", got)
 	})
 }
