de4dbe3da37badd23d7eb780b039c701e084c4b3
Filip Aleksic <faleksic@gitlab.com>
2026-05-26 11:35:24 +0200

    chore(code): unneeded function

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -163,12 +163,6 @@ func PresentInStringSlice(hay []string, needle string) bool {
 	return slices.Contains(hay, needle)
 }
 
-// PresentInIntSlice take a Hay (Slice of Ints) and a Needle (int)
-// and returns true based on whether or not the Needle is present in the hay.
-func PresentInIntSlice(hay []int, needle int) bool {
-	return slices.Contains(hay, needle)
-}
-
 // CommonElementsInStringSlice takes 2 Slices of Strings and returns a Third Slice
 // that is the common elements between the first 2 Slices.
 func CommonElementsInStringSlice(s1 []string, s2 []string) []string {
